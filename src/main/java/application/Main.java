389961ead4b649af918ff3209dc8d2cd475b2f2a
Olaf Roeder <github@olaf-roeder.de>
2021-01-05 17:16:50 +0100

    SSCCE without ProGuard;

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,7 +1,15 @@
 package application;
 
+import java.net.URL;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        String directory = "testDirectory";
+        URL testDirectory = Main.class.getResource(directory);
+
+        System.out.println("Testing file attributes, following line should show the URL to a directory 'testDirectory'");
+        System.out.println(testDirectory.toExternalForm());
     }
 }
